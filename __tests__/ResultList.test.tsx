141bd78120e3b5b22ed409a8d86ebcf4e33ffa53
Antonio Balsamo <a.balsamo88@gmail.com>
2023-11-07 15:40:42 +0100

    Test fix

--- __tests__/ResultList.test.tsx
+++ __tests__/ResultList.test.tsx
@@ -1,18 +1,17 @@
-import renderer from 'react-test-renderer';
-import { ResultList } from '../src/components/ResultList/ResultList';
-import React from 'react';
+import ResultList from "@/components/ResultList/ResultList"
+import { render } from "@testing-library/react"
 
-test('se nelle props ricevo una lista vuota, il div #resultList non dovrà avere figli', () => {
-    const results = renderer.create(<ResultList result={[]} />);
-    const document = results.toJSON();
-    const div = document?.children.find( => child.props.id == "resultList");
-expect(div?.children.length).toBe(0);
-});
+test('Se nella prop result di ResultList ricevo una lista vuota, il div #resultList non dovrà avere figli', () => {
+  const { container } = render(<ResultList result={[]} />);
+  const div = container.querySelector('#resultList');
+  console.log(`Rilevato ${div?.children.length}`);
+  expect(div?.children.length).toBe(0);
+})
 
-test('se nelle props ottengo 1 solo risultato, mi aspetto che il div #resultList contenga 1 solo elemento e che rispecchi i valori delle props.', () => {
-    const mockProps = [{ title: "The Lord of the Rings", overview: "bla bla bla", cover: "", id: 1 }];
-    const results = renderer.create(<ResultList result={mockProps} />);
-    const document = results.toJSON();
-    const div = document?.children.find(child => child.props.id == "resultList");
-    expect(div?.children.length).toBe(1);
-});
+test('Se nella prop result di ResultList ricevo una lista di 1 elemento, mi aspetto che il div #resultList contenga 1 solo figlio', () => {
+  const movies = [{ title: "The Lord of the Rings", overview: "bla bla bla", cover: "", id: 1 }];
+  const { container } = render(<ResultList result={movies} />);
+  const div = container.querySelector('#resultList');
+  console.log(`Rilevato ${div?.children.length}`);
+  expect(div?.children.length).toBe(1);
+})
--- src/components/ResultList/ResultList.tsx
+++ src/components/ResultList/ResultList.tsx
@@ -3,28 +3,22 @@ import { Movie } from '@/models/interfaces/movie';
 
 interface ResultsProps {
     result: Movie[];
-
 }
 
 export const ResultList: React.FC<ResultsProps> = (props) => {
     return <>
         <span className='mb-8 mt-1'>{props.result.length} results</span>
 
-        <div className="flex flex-col gap-y-4">
-
+        <div id="resultList" className="flex flex-col gap-y-4">
             {
                 props.result.map((movies: Movie) => {
-
-
                     return <div key={movies.id} className="flex flex-row gap-x-4 max-w-sm sm:max-w-3xl">
-
-                        <img className="h-32 min-w-32" src={movies.cover} alt="" />
-                        <div className='flex flex-col'>
-                            <h1 className="font-black text-xl">{movies.title}</h1>
-                            <div className=" ">{movies.overview}</div>
-                        </div>
-                    </div>
-
+                               <img className="h-32 min-w-32" src={movies.cover} alt="" />
+                               <div className='flex flex-col'>
+                                   <h1 className="font-black text-xl">{movies.title}</h1>
+                                   <div className=" ">{movies.overview}</div>
+                               </div>
+                           </div>
                 })
             }
         </div>
